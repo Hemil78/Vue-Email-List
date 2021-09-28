8e21be84da0860d4b8f904cd863bc15c762ff9bb
Hemil78 <zanattahemil@gmail.com>
2021-09-28 15:53:59 +0200

    Vue Mail List

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//Attraverso l'apposita API di Boolean
+//https://flynn.boolean.careers/exercises/api/random/mail
+//generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+//Bonus
+//Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        emails: []
+    },
+    methods: {
+
+    },
+    mounted() {
+        /* per generare mail randomfino a 10 */
+        for( let i = 1; i <= 10; i++) {
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (elm) => {
+                this.emails.push(elm.data.response);
+            });
+        }
+
+    }
+});
